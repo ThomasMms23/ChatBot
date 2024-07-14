d9e0de118c100e745db9fdca4090ce4eaaad5431
Thomas <monmousseau.thomas@gmail.com>
2024-07-14 20:28:44 +0200

    remove some code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/hero";
 import ChatbotWrapper from "@/components/ChatbotWrapper";
-import Footer from "@/components/footer";
 
 export default function Home() {
   return (
